e152a59ea07e817376ce134782410017914a2010
IsaacVera10 <isaac.vera@utec.edu.pe>
2023-08-25 01:05:26 -0500

    readRecord(pos) v1

--- Lab1/p2.cpp
+++ Lab1/p2.cpp
@@ -18,7 +18,68 @@ struct Alumno{
     }
 };
 
+class VariableRecordFile{
+private:
+    string filename;
+public:
+    explicit VariableRecordFile(string filename){
+        this->filename = filename;
+    }
+
+    void write(Alumno alumno){
+//        ofstream file(filename, ios::binary | ios::app);
+//        file.write(alumno.Nombre.c_str(), alumno.Nombre.size());
+//        file.write(alumno.Apellidos.c_str(), alumno.Apellidos.size());
+//        file.write(alumno.Carrera.c_str(), alumno.Carrera.size());
+//        file.write((char*)&alumno.mensualidad, sizeof(alumno.mensualidad));
+//        file.close();
+    }
+
+    vector<Alumno> read(){
+//        vector<Alumno> alumnos;
+//        ifstream file(filename, ios::binary);
+//        while(!file.eof()){
+//            Alumno alumno;
+//            char buffer[100];
+//            file.read(buffer, 100);
+//            alumno.Nombre = buffer;
+//            file.read(buffer, 100);
+//            alumno.Apellidos = buffer;
+//            file.read(buffer, 100);
+//            alumno.Carrera = buffer;
+//            file.read((char*)&alumno.mensualidad, sizeof(alumno.mensualidad));
+//            alumnos.push_back(alumno);
+//        }
+//        file.close();
+//        return alumnos;
+    }
+
+    Alumno readRecord(int pos){
+        ifstream fm("cabacera.bin", ios::binary);//abro el archivo, que será nuestra metadata, en donde almacenaré la posición y el tamaño de cada registro.
+        fm.seekg(pos*sizeof(int)*2);//Cada registro tiene 2 enteros, pos y tamaño. Nos movemos a la posición del registro que queremos leer.
+
+        //Obtenemos las características del record[pos]
+        int pos_fisica, tam_reg;
+        fm.read((char*)&pos_fisica, sizeof(int));//Leemos la posición física del registro que queremos leer.
+        fm.read((char*)&tam_reg, sizeof(int));//Leemos el tamaño del registro que queremos leer.
+
+        fm.close();//Es todo lo que quería de la metadata
+
+        //Procedemos a trabajar con el archivo de registros
+        ifstream file(filename, ios::binary);//abro el archivo
+        file.seekg(pos_fisica);//Nos movemos a la posición del registro que queremos leer.
+
+        Alumno record;
+        file.read((char*)&record, tam_reg);//Leemos el registro que queremos leer.
+        file.close();//Cerramos el archivo
+
+        return record;
+    }
+
+};
+
 int main(){
+    VariableRecordFile file1("data_variable.bin");
     Alumno a1;
     a1.Nombre = "David";
     a1.Apellidos = "Gonzalez";
