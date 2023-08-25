31f9b213e0254648cec13b1caaecb198c80de073
IsaacVera10 <isaac.vera@utec.edu.pe>
2023-08-24 22:35:47 -0500

    overload size

--- Lab1/p2.cpp
+++ Lab1/p2.cpp
@@ -7,8 +7,24 @@
 
 using namespace std;
 
-int main(){
+struct Alumno{
+    string Nombre;
+    string Apellidos;
+    string Carrera;
+    float mensualidad;
 
+    size_t size(){
+        return Nombre.size() + Apellidos.size() + Carrera.size() + sizeof(mensualidad);
+    }
+};
+
+int main(){
+    Alumno a1;
+    a1.Nombre = "David";
+    a1.Apellidos = "Gonzalez";
+    a1.Carrera = "Ing. Sistemas";
+    a1.mensualidad = 1000.0;
+    cout<<a1.size()<<endl;
 
 
     return 0;
