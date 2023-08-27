9304a699b58c90f6fafa9ecdbaf3b535a5dafcd5
Luis-David Torres <luis.torres.o@utec.edu.pe>
2023-08-26 20:20:21 -0500

    Delete author david

--- Lab1/p1.cpp
+++ Lab1/p1.cpp
@@ -1,6 +1,3 @@
-//
-// Created by david on 19/08/2023.
-//
 #include <iostream>
 #include <fstream>
 #include <utility>
--- Lab1/p2.cpp
+++ Lab1/p2.cpp
@@ -1,6 +1,3 @@
-//
-// Created by david on 24/08/2023.
-//
 #include <fstream>
 #include <iostream>
 #include <vector>
